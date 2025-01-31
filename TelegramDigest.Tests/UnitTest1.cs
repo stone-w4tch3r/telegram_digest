679781320c7a5f400e1ec7704668e7618c22dda3
stone-w4tch3r <100294019+stone-w4tch3r@users.noreply.github.com>
2025-01-31 19:16:53 +0500

    razor frontend and tests projs createdˆ

---
+++ TelegramDigest.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace TelegramDigest.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}

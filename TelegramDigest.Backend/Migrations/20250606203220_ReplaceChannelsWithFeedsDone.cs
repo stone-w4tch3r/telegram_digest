908f618b7d655554dbda5582f8537a9f906ec435
stone-w4tch3r <100294019+stone-w4tch3r@users.noreply.github.com>
2025-06-07 01:32:40 +0500

    channels -> feeds migration

---
+++ TelegramDigest.Backend/Migrations/20250606203220_ReplaceChannelsWithFeedsDone.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TelegramDigest.Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReplaceChannelsWithFeedsDone : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ChannelsJson",
+                table: "DigestStatuses",
+                newName: "FeedsJson");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FeedsJson",
+                table: "DigestStatuses",
+                newName: "ChannelsJson");
+        }
+    }
+}

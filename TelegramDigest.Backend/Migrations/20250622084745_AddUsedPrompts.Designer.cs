a01711a2751972697c64d7fcd87d997910d2f3a3
stone-w4tch3r <100294019+stone-w4tch3r@users.noreply.github.com>
2025-06-22 13:48:13 +0500

    AddUsedPrompts migration

--- TelegramDigest.Backend/Migrations/20250622084745_AddUsedPrompts.Designer.cs
+++ TelegramDigest.Backend/Migrations/20250622084745_AddUsedPrompts.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TelegramDigest.Backend.Db;
 
@@ -10,9 +11,11 @@ using TelegramDigest.Backend.Db;
 namespace TelegramDigest.Backend.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250622084745_AddUsedPrompts")]
+    partial class AddUsedPrompts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.1");
---
+++ TelegramDigest.Backend/Migrations/20250622084745_AddUsedPrompts.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TelegramDigest.Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUsedPrompts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UsedPrompts",
+                table: "Digests",
+                type: "json",
+                maxLength: 4096,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UsedPrompts",
+                table: "Digests");
+        }
+    }
+}

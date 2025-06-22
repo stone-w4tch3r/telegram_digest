484ddab97dd0529a949d60925910de52390f1397
stone-w4tch3r <100294019+stone-w4tch3r@users.noreply.github.com>
2025-06-22 13:52:44 +0500

    update migration

--- TelegramDigest.Backend/Migrations/20250622084745_AddUsedPrompts.cs
+++ TelegramDigest.Backend/Migrations/20250622084745_AddUsedPrompts.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
@@ -10,21 +10,30 @@ namespace TelegramDigest.Backend.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            if (!migrationBuilder.IsSqlite())
+            {
+                throw new NotImplementedException(
+                    "Migration for this database provider is not implemented."
+                );
+            }
             migrationBuilder.AddColumn<string>(
                 name: "UsedPrompts",
                 table: "Digests",
                 type: "json",
                 maxLength: 4096,
                 nullable: false,
-                defaultValue: "");
+                defaultValue: ""
+            );
+            // Set default value for all existing rows as a JSON dictionary
+            migrationBuilder.Sql(
+                "UPDATE Digests SET UsedPrompts = '{\"PostSummary\":\"unknown\",\"PostImportance\":\"unknown\",\"DigestSummary\":\"unknown\"}'"
+            );
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropColumn(
-                name: "UsedPrompts",
-                table: "Digests");
+            migrationBuilder.DropColumn(name: "UsedPrompts", table: "Digests");
         }
     }
 }
